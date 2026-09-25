a3b6e10258180f3107da5a29f5a2f73294866209
santiago-carmona <santicar0607@gmail.com>
2026-09-24 18:24:06 -0600

    Practica 1
    Se realizo la practica #1 donde se crean diferentes archivos en formato java, css y en su mayoria html, se crean las paginas y tambien el diseño de la pagina

--- tiendaTech/src/main/java/tiendaTech/tienda/ProjectConfig.java
+++ tiendaTech/src/main/java/tiendaTech/tienda/ProjectConfig.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tiendaTech.tienda;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 /**
  *
  * @author santi
  */
-public class ProjectConfig {
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/ejemplo2").setViewName("ejemplo2");
+        registry.addViewController("/multimedia").setViewName("multimedia");
+        registry.addViewController("/iframes").setViewName("iframes");
+        registry.addViewController("/multimedia").setViewName("multimedia");
+        registry.addViewController("/login").setViewName("/login");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
     
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
 }
